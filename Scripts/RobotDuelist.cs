6c3fe13c1f98961b911477130ebf4c430f4cb667
AjinGixtas <tdinhan999@gmail.com>
2024-06-09 05:20:00 +0700

    Half-ass attempt to work

--- Scripts/RobotDuelist.cs
+++ Scripts/RobotDuelist.cs
@@ -5,14 +5,14 @@ public partial class RobotDuelist : Enemy
 	[Export] float JUMP_FORCE;
 	[Export] Sprite2D SPRITE; [Export] AnimationTree ANIMATION_TREE; 
 	[Export] Timer jumpAttackCooldownTimer, jumpDurationTimer; 
-	Vector2 OldPlayerPosition = new(1000, 1000); // A simple duct-tape fix ;D
 	float GRAVITY = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 	bool canJump = true, isJumping = false;
+	bool IsJumping { get { return isJumping; } set { isJumping = value; ANIMATION_TREE.Set("parameters/conditions/isJumping", value); }}
 	KinematicCollision2D c_collision; Node2D c_collisionNode;
 	SurfaceType SurfaceCurrentlyInContact {
 		get { return surfaceCurrentlyInContact; }
 		set {
-			if(surfaceCurrentlyInContact != SurfaceType.NONE) { isJumping = false; }
+			if(surfaceCurrentlyInContact != SurfaceType.NONE) { isJumping = false; ANIMATION_TREE.Set("parameters/conditions/isOnSurface", true); } else { ANIMATION_TREE.Set("parameters/conditions/isOnSurface", false); }
 			if (c_collision != null) velocity = -c_collision.GetNormal();
 			surfaceCurrentlyInContact = value;
 		}
@@ -32,15 +32,12 @@ public partial class RobotDuelist : Enemy
 	{
 		if (canJump)
 		{
-			OldPlayerPosition = PLAYER.Position;
-			Velocity = (PLAYER.Position - Position).Normalized() * JUMP_FORCE;
+			velocity = (PLAYER.Position - Position).Normalized() * JUMP_FORCE;
 			canJump = false; isJumping = true;
 			jumpAttackCooldownTimer.Start(); jumpDurationTimer.Start();
 		}
-		if((Position - OldPlayerPosition).LengthSquared() < 2 && isJumping) {
-			Velocity = new(Velocity.X / 2, 0);
-		}
-		c_collision = MoveAndCollide(Velocity);
+		GD.Print(canJump, isJumping, isJumping, (PLAYER.Position - Position).Normalized(), (PLAYER.Position - Position).Normalized() * JUMP_FORCE, SurfaceCurrentlyInContact, velocity, JUMP_FORCE);
+		c_collision = MoveAndCollide(velocity);
 		if(c_collision != null) {
 			c_collisionNode = c_collision.GetCollider() as Node2D;
 			if (c_collisionNode.IsInGroup("Floor")) { SurfaceCurrentlyInContact = SurfaceType.FLOOR; }
@@ -48,8 +45,7 @@ public partial class RobotDuelist : Enemy
 			else if (c_collisionNode.IsInGroup("WallRight")) { SurfaceCurrentlyInContact = SurfaceType.WALL_RIGHT; }
 			else { SurfaceCurrentlyInContact = SurfaceType.NONE; }
 		} else { SurfaceCurrentlyInContact = SurfaceType.NONE; }
-		velocity = new(velocity.X, isJumping ? velocity.Y + GRAVITY * (float)Engine.TimeScale : velocity.Y);
-		GD.Print(canJump, isJumping, isJumping, OldPlayerPosition, SurfaceCurrentlyInContact, velocity);
+		velocity = new(surfaceCurrentlyInContact == SurfaceType.NONE ? velocity.X : velocity.X * .97f, isJumping ? velocity.Y : velocity.Y + GRAVITY * (float)delta * (float)Engine.TimeScale);
 	}
 	public void OnJumpAttackCooldownTimerTimeout() { canJump = true; }
 	public void OnJumpDurationTimerTimeout() { isJumping = false; }
